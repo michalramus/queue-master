af19a966e33e50d054ee06eefabe965446ec523a
Michal Ramus <ramus.michal21@gmail.com>
2024-06-10 10:47:42 +0200

    feat: Pass arguments to header by {children}

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,9 @@
-export default function Header({ title }: { title: string }) {
+import { ReactNode } from "react";
+
+export default function Header({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full rounded-full border-2 bg-white bg-opacity-10 p-6 text-center">
-      <h1 className="text-5xl">{title}</h1>
+    <div className="w-full rounded-full font-semibold border-2 bg-white bg-opacity-10 p-6 text-center">
+      <h1 className="text-5xl">{children}</h1>
     </div>
   );
 }
