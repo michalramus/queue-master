370a356be1520f4a59c97c808e97dc8bd593e499
Michal Ramus <ramus.michal21@gmail.com>
2024-06-21 19:42:17 +0200

    feat: Move small buttons from clients table into individual components

--- frontend/app/seat/ClientTableRow.tsx
+++ frontend/app/seat/ClientTableRow.tsx
@@ -1,4 +1,6 @@
 import { ClientNumber, setClientAsInService, removeClient } from "@/api/clients";
+import SmallRejectButton from "@/components/buttons/SmallRejectButton";
+import SmallAcceptButton from "@/components/buttons/SmallAcceptButton";
 import { useMutation, useQueryClient, QueryClient } from "@tanstack/react-query";
 
 export default function ClientTableRow({
@@ -34,50 +36,8 @@ export default function ClientTableRow({
                 {new Date(clientNumber.creationDate).toLocaleDateString("en-EN", { hour12: false })}
             </td>
             <td className="px-6 py-4">
-                <button
-                    type="button"
-                    className="me-2 inline-flex items-center rounded-full bg-red-600 p-2.5 text-center text-sm font-medium text-white hover:bg-red-700"
-                    aria-label="Reject new client"
-                    onClick={() => deleteClient.mutate({ clientNumber })}
-                >
-                    <svg
-                        className="h-4 w-4"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 10 10"
-                    >
-                        <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="1.7"
-                            d="M1 1 L9 9 M9 1 L1 9"
-                        />
-                    </svg>
-                </button>
-                <button
-                    type="button"
-                    className="me-2 inline-flex items-center rounded-full bg-green-600 p-2.5 text-center text-sm font-medium text-white hover:bg-green-700"
-                    aria-label="Accept new client"
-                    onClick={() => clientInService.mutate({ clientNumber, seat })}
-                >
-                    <svg
-                        className="h-4 w-4"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 14 10"
-                    >
-                        <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2.5"
-                            d="M1 5h12m0 0L9 1m4 4L9 9"
-                        />
-                    </svg>
-                </button>
+                <SmallRejectButton onClick={() => deleteClient.mutate({ clientNumber })} />
+                <SmallAcceptButton onClick={() => clientInService.mutate({ clientNumber, seat })} />
             </td>
         </tr>
     );
