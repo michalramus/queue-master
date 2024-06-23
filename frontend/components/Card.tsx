f048fb30193773e9abd87dada360228db2d55a87
Michal Ramus <ramus.michal21@gmail.com>
2024-06-23 14:32:02 +0200

    bug: Fix color injection into components

---
+++ frontend/components/Card.tsx
@@ -0,0 +1,15 @@
+export default function Card({
+    children,
+    className,
+}: {
+    children: React.ReactNode;
+    className?: string;
+}) {
+    return (
+        <div
+            className={`block max-w-sm rounded-lg border border-gray-700 bg-gray-800 p-6 shadow ${className}`}
+        >
+            {children}
+        </div>
+    );
+}
--- frontend/components/buttons/Button.tsx
+++ frontend/components/buttons/Button.tsx
@@ -7,17 +7,32 @@ export default function Button({
 }: {
     children: React.ReactNode;
     onClick: () => void;
-    color?: string;
+    color?: "blue" | "green" | "red";
     disabled?: boolean;
     className?: string;
 }) {
+
+    let colorClass = "";
+    switch (color) {
+        case "blue":
+            colorClass = "bg-blue-600 hover:bg-blue-700";
+            break;
+        case "green":
+            colorClass = "bg-green-600 hover:bg-green-700";
+
+            break;
+        case "red":
+            colorClass = "bg-red-600 hover:bg-red-700";
+            break;
+    }
+
     return (
         <button
             type="button"
             onClick={onClick}
-            className={`mb-2 me-2 rounded-lg px-5 py-2.5 text-sm font-medium text-white bg-${color}-600 hover:bg-${color}-700 ${disabled && "cursor-not-allowed"} ${className}`}
+            className={`mb-2 me-2 rounded-lg px-5 py-2.5 text-sm font-medium text-white ${colorClass} ${disabled && "cursor-not-allowed"} ${className}`}
             disabled={disabled}
-       >
+        >
             {children}
         </button>
     );
