1b97910e7826efa0355c6a364d3ca79999748d4d
Michal Ramus <ramus.michal21@gmail.com>
2024-06-25 09:19:52 +0200

    feat: Add class injection into CurrentNumberWidget component

--- frontend/app/tv/CurrentNumberWidget.tsx
+++ frontend/app/tv/CurrentNumberWidget.tsx
@@ -1,14 +1,14 @@
 interface CurrentNumberWidgetProps {
     number: number | string;
     seat: number | string;
+    className?: string;
 }
 
-export default function CurrentNumberWidget({
-    number,
-    seat,
-}: CurrentNumberWidgetProps) {
+export default function CurrentNumberWidget({ number, seat, className }: CurrentNumberWidgetProps) {
     return (
-        <div className="w-full rounded-md border-2 bg-white bg-opacity-5 p-6 text-center lg:w-6/12">
+        <div
+            className={`w-full rounded-md border-2 bg-white bg-opacity-5 p-6 text-center ${className}`}
+        >
             <h1 className="pb-5 text-5xl">
                 Current number: <span className="font-bold">{number}</span>
             </h1>
