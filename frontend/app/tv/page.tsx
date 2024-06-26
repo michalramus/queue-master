fc84cf25aa85b711d16db068f91d27a3549c0f78
Michał Ramus <79167836+michalramus@users.noreply.github.com>
2024-06-26 11:16:51 +0200

    . (#1)
    * feat: Add queuing for changing multiple clients state at the same time
    
    * bug: Fix showing clients from queue in TV page
    
    * feat: Allow changing seat and categoryIds variables inside seat page
    
    * feat: Implement "call again" ws channel handling in TV page

--- frontend/app/tv/page.tsx
+++ frontend/app/tv/page.tsx
@@ -2,35 +2,21 @@
 
 import Header from "@/components/Header";
 import { ClientNumber } from "../../api/clients";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import CurrentNumberWidget from "./CurrentNumberWidget";
 import { io } from "socket.io-client";
 import { wsClientEvents } from "@/api/clients";
 import ClientNumbersHistory from "./ClientNumbersHistoryTable";
 
-async function playAudio(client: ClientNumber) {
-    //TODO wait for audio to finish
-
-    const number = new Audio(process.env.NEXT_PUBLIC_API + "/audio-samples/pl/" + client.number); //TODO: Move to settings
-    number.play();
-    await new Promise((resolve, reject) => {
-        number.onerror = reject;
-        number.onended = resolve;
-    });
-    
-    const seat = new Audio(process.env.NEXT_PUBLIC_API + "/audio-samples/pl/" + "SEAT"+ client.seat); //TODO: Move to settings
-    seat.play();
-    await new Promise((resolve, reject) => {
-        seat.onerror = reject;
-        seat.onended = resolve;
-    });
-}
-
 export default function TVPage() {
-    const [currentClient, setCurrentClient] = useState<ClientNumber | null>();
+    const [currentClient, setCurrentClient] = useState<ClientNumber | null>(null);
+    const currentClientRef = useRef<ClientNumber | null>(null);
 
     const [previousClients, setPreviousClients] = useState<ClientNumber[]>([]);
-    const [newClient, setNewClient] = useState<ClientNumber | null>();
+    const previousClientsRef = useRef<ClientNumber[] | null>(null);
+
+    const [newClientsQueue, setNewClientsQueue] = useState<ClientNumber[]>([]);
+    const isShowNewClientsRunning = useRef(false); //Protect from multiple calls at the same time - something like a mutex
 
     const maxHistory = 5; // TODO: Move to settings
 
@@ -38,42 +24,76 @@ export default function TVPage() {
     useEffect(() => {
         const socket = io(process.env.NEXT_PUBLIC_API ?? "");
 
-        function onClientInService(client: ClientNumber) {
-            setNewClient(client);
+        function onClientToShow(client: ClientNumber) {
+            setNewClientsQueue((e) => [...e, client]);
         }
 
-        socket.on(wsClientEvents.ClientInService, onClientInService);
+        socket.on(wsClientEvents.ClientInService, onClientToShow);
+        socket.on(wsClientEvents.ClientCallAgain, onClientToShow);
         return () => {
-            socket.off(wsClientEvents.ClientInService, onClientInService);
+            socket.off(wsClientEvents.ClientInService, onClientToShow);
+            socket.off(wsClientEvents.ClientCallAgain, onClientToShow);
         };
     }, []);
 
+    //Update Refs
+    useEffect(() => {
+        currentClientRef.current = currentClient;
+        previousClientsRef.current = previousClients;
+    }, [currentClient, previousClients]);
+
     /**
      * Play audio and update previousClients and currentClient in order to show it on the screen
+     * Function works in a loop until newClientsQueue is empty, but it's protected from multiple calls at the same time
+     * If number is already in previousClients it won't be added again, but audio will be played
      */
-    function showNewClient() {
-        let prevClients = previousClients; // Copy previousClients - useEffect updates are not immediate
+    const showNewClients = useCallback(async () => {
+        //Protect from multiple calls at the same time
+        if (isShowNewClientsRunning.current) {
+            return;
+        }
+        isShowNewClientsRunning.current = true;
 
-        if (currentClient) {
-            prevClients.unshift(currentClient);
+        for (const client of newClientsQueue) {
+            if (
+                currentClientRef.current?.number != client.number &&
+                previousClientsRef.current?.findIndex((e) => e.number === client.number) === -1
+            ) {
+                //Update previousClients and currentClient
+                setPreviousClients((e) => {
+                    const newClients = currentClientRef.current
+                        ? [currentClientRef.current, ...e]
+                        : [...e];
+                    return newClients.slice(0, maxHistory);
+                });
+                setCurrentClient(client);
+            }
+
+            //Play audio
+            const number = new Audio(
+                process.env.NEXT_PUBLIC_API + "/audio-samples/pl/" + client.number,
+            ); //TODO: Move to settings
+            const seat = new Audio(
+                process.env.NEXT_PUBLIC_API + "/audio-samples/pl/" + "SEAT" + client.seat,
+            ); //TODO: Move to settings
+            number.play();
+            await new Promise((resolve) => {
+                number.onended = resolve;
+            });
+            seat.play();
+            await new Promise((resolve) => {
+                seat.onended = resolve;
+            });
+
+            setNewClientsQueue((e) => e.slice(1));
         }
 
-        if (prevClients.length > maxHistory) {
-            prevClients = prevClients.slice(0, -1); // Remove last element
-        }
+        isShowNewClientsRunning.current = false;
+    }, [currentClientRef, newClientsQueue, previousClientsRef, isShowNewClientsRunning]);
 
-        // Update states
-        setCurrentClient(newClient);
-        setPreviousClients(prevClients);
-
-        playAudio(newClient!);
-
-        setNewClient(null);
-    }
-
-    if (newClient) {
-        showNewClient();
-    }
+    useEffect(() => {
+        showNewClients();
+    }, [newClientsQueue, showNewClients]);
 
     return (
         <main className="flex min-h-screen flex-col items-center p-24">
