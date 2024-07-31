ed2280a55a7633d0dca87953a4bdfbd2561f11a7
Michal Ramus <ramus.michal21@gmail.com>
2024-07-31 15:30:41 +0200

    feat(api): Add required dependencies to Auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -7,9 +7,10 @@ import { AuthController } from "./auth.controller";
 import { JwtModule } from "@nestjs/jwt";
 import { JwtRefreshTokenStrategy } from "./strategies/jwtRefreshToken.strategy";
 import { JwtStrategy } from "./strategies/jwt.strategy";
+import { DevicesModule } from '../devices/devices.module';
 
 @Module({
-    imports: [UsersModule, PassportModule, JwtModule],
+    imports: [UsersModule, PassportModule, JwtModule, DevicesModule],
     providers: [AuthService, LocalStrategy, JwtStrategy, JwtRefreshTokenStrategy],
     controllers: [AuthController],
 })
