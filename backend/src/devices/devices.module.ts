1674933e96e0dc34896cef18089af997782c5b4d
Michal Ramus <ramus.michal21@gmail.com>
2024-07-31 15:20:28 +0200

    bug(api): Add required dependencies to Devices module

--- backend/src/devices/devices.module.ts
+++ backend/src/devices/devices.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DevicesService } from './devices.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  providers: [DevicesService]
+  imports: [DatabaseModule],
+  providers: [DevicesService],
+  exports: [DevicesService]
 })
 export class DevicesModule {}
