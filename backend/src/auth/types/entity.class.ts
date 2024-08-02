66188c41dc2295564da622a3fc1c588016e08bd3
Michal Ramus <ramus.michal21@gmail.com>
2024-08-02 14:36:55 +0200

    feat(api): Add 'name' field to JWT payload

--- backend/src/auth/types/entity.class.ts
+++ backend/src/auth/types/entity.class.ts
@@ -4,19 +4,23 @@
 export class Entity {
     id: number;
     type: "User" | "Device";
+    name: string;
+
+    constructor(id: number, type: "User" | "Device", name: string) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+    }
 
     /**
-     * 
-     * @param req should match schema: req.user:{sub, type}
+     *
+     * @param req should match schema: req.user:{sub, type, name}
      */
-    convertFromReq(req: any) {
-        this.id = req.user.id;
-        this.type = req.user.type;
-
-        return this;
+    static convertFromReq(req: { user: { id: number; type: "User" | "Device"; name: string } }) {
+        return new Entity(req.user.id, req.user.type, req.user.name); //IMPORTANT: If changing this, update also jwt strategies
     }
 
     getJwtPayload() {
-        return { sub: this.id, type: this.type };
+        return { sub: this.id, type: this.type, name: this.name };  //IMPORTANT: If changing this, update also jwt strategies
     }
 }
