4f8734f0b19f8e0b5db54924ee790f341b8ead6d
Michal Ramus <ramus.michal21@gmail.com>
2024-08-02 20:44:49 +0200

    feat(gui): Add Loading indicator to kiosk page

--- frontend/app/kiosk/page.tsx
+++ frontend/app/kiosk/page.tsx
@@ -7,16 +7,17 @@ import { getCategories } from "@/api/categories";
 import { Category } from "@/api/categories";
 
 export default function KioskPage() {
-    const { data: categories, isLoading } = useQuery({ queryKey: ["categories"], queryFn: getCategories });
+    const { data: categories, isLoading: isLoadingCategories } = useQuery({ queryKey: ["categories"], queryFn: getCategories });
 
     return (
         <main className="flex min-h-screen flex-col items-center p-24">
             <Header>Queue System</Header>
 
             <div className="mt-20 flex w-full flex-col items-center">
-                {categories?.map((category: Category) => {
+                {isLoadingCategories && <p>Loading...</p>}
+                {categories && (categories?.map((category: Category) => {
                     return <NumberGetterButton key={category.id} category={category} />;
-                })}
+                }))}
             </div>
         </main>
     );
