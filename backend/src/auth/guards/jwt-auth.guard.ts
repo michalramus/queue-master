00d0f3176af5234f5ed5857b4ee1416f1cb56621
Michal Ramus <ramus.michal21@gmail.com>
2024-08-02 11:20:41 +0200

    feat(api): Add logging to JWT Guards

--- backend/src/auth/guards/jwt-auth.guard.ts
+++ backend/src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Injectable, Logger, UnauthorizedException, ExecutionContext } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard("jwt") {
+    private logger = new Logger(JwtAuthGuard.name);
+
+    handleRequest(err, user, info, context) {
+        const request = context.switchToHttp().getRequest();
+        const { method, ip, url } = request;
+
+        if (err || !user) {
+            this.logger.warn(`${info} | ${method} ${url} ${ip}`, `Token: ${request.headers.authorization}`);
+            throw new UnauthorizedException(info);
+        }
+        return user;
+    }
+}
--- backend/src/auth/guards/jwt-refreshToken-auth.guard.ts
+++ backend/src/auth/guards/jwt-refreshToken-auth.guard.ts
@@ -1,5 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
+import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class JwtRefreshTokenAuthGuard extends AuthGuard('jwtRefreshToken') {}
+export class JwtRefreshTokenAuthGuard extends AuthGuard("jwtRefreshToken") {
+    private logger = new Logger(JwtRefreshTokenAuthGuard.name);
+
+    handleRequest(err, user, info, context) {
+        const request = context.switchToHttp().getRequest();
+        const { method, ip, url } = request;
+
+        if (err || !user) {
+            this.logger.warn(`${info} | ${method} ${url} ${ip}`, `Token: ${request.headers.authorization}`);
+            throw new UnauthorizedException(info);
+        }
+        return user;
+    }
+}
