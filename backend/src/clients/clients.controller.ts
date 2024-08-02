920766d52d5671134c84eed497d248147a9251ef
Michal Ramus <ramus.michal21@gmail.com>
2024-08-02 20:06:28 +0200

    feat(api): Add logging to Clients Service

--- backend/src/clients/clients.controller.ts
+++ backend/src/clients/clients.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, UseGuards, Request } from "@nestjs/common";
 import { ClientsService } from "./clients.service";
 import { CreateClientDto } from "./dto/create-client.dto";
 import { UpdateClientDto } from "./dto/update-client.dto";
 import { Roles } from "../auth/roles.decorator";
 import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 import { RolesGuard } from "../auth/guards/roles.guard";
+import { Entity } from "src/auth/types/entity.class";
 
 @Controller("clients")
 export class ClientsController {
@@ -13,8 +14,8 @@ export class ClientsController {
     @Post()
     @Roles(["Device", "User", "Admin"])
     @UseGuards(JwtAuthGuard, RolesGuard)
-    create(@Body(ValidationPipe) createClientDto: CreateClientDto) {
-        return this.clientsService.create(createClientDto);
+    create(@Body(ValidationPipe) createClientDto: CreateClientDto, @Request() req) {
+        return this.clientsService.create(createClientDto, Entity.convertFromReq(req));
     }
 
     @Get()
@@ -27,21 +28,21 @@ export class ClientsController {
     @Patch(":id")
     @Roles(["User", "Admin"])
     @UseGuards(JwtAuthGuard, RolesGuard)
-    update(@Param("id") id: string, @Body(ValidationPipe) updateClientDto: UpdateClientDto) {
-        return this.clientsService.update(id, updateClientDto);
+    update(@Param("id") id: string, @Body(ValidationPipe) updateClientDto: UpdateClientDto, @Request() req) {
+        return this.clientsService.update(id, updateClientDto, Entity.convertFromReq(req));
     }
 
     @Post(":id/call-again")
     @Roles(["User", "Admin"])
     @UseGuards(JwtAuthGuard, RolesGuard)
-    findOne(@Param("id") id: string) {
-        return this.clientsService.callAgain(id);
+    findOne(@Param("id") id: string, @Request() req) {
+        return this.clientsService.callAgain(id, Entity.convertFromReq(req));
     }
 
     @Delete(":id")
     @Roles(["User", "Admin"])
     @UseGuards(JwtAuthGuard, RolesGuard)
-    remove(@Param("id") id: string) {
-        return this.clientsService.remove(id);
+    remove(@Param("id") id: string, @Request() req) {
+        return this.clientsService.remove(id, Entity.convertFromReq(req));
     }
 }
