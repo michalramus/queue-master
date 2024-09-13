2a0d575811132276b64df96cb89c53c5208178d0
Michal Ramus <ramus.michal21@gmail.com>
2024-09-13 20:09:08 +0200

    feat(api): Update CORS config to allow http only cookies

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "./app.module";
 import { ConfigModule } from "@nestjs/config";
 import { LoggingInterceptor } from "./middleware/logging.interceptor";
 import * as cookieParser from 'cookie-parser';
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function bootstrap() {
     ConfigModule.forRoot();
@@ -10,7 +11,13 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalInterceptors(new LoggingInterceptor());
     app.use(cookieParser());
-    app.enableCors();
+
+    const corsOptions: CorsOptions = { 
+        origin: process.env.FRONTEND_URL,
+        credentials: true, 
+      };
+
+    app.enableCors(corsOptions);
     await app.listen(process.env.PORT);
 }
 bootstrap();
