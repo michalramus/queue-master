9cf1842ac899e9cd4ef7a9425b3838ee236b8a7a
Michal Ramus <ramus.michal21@gmail.com>
2024-09-14 20:13:18 +0200

    ref(gui): Move <main> tag to login page from LoginForm component

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,5 +1,9 @@
 import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
-    return (<LoginForm />);
+    return (
+        <main className="flex min-h-screen flex-col items-center p-24">
+            <LoginForm />
+        </main>
+    );
 }
