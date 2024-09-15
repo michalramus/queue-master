985806d31702ca5ccc43eacdf0308b924d625e1e
Michal Ramus <ramus.michal21@gmail.com>
2024-09-15 10:17:54 +0200

    feat(gui): Add roles protection to kiosk and seat pages

--- frontend/app/kiosk/page.tsx
+++ frontend/app/kiosk/page.tsx
@@ -5,6 +5,9 @@ import NumberGetterButton from "./NumberGetterButton";
 import { useQuery } from "@tanstack/react-query";
 import { getCategories } from "@/utils/api/categories";
 import { Category } from "@/utils/api/categories";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import protectPage from "@/utils/protectPage";
 
 export default function KioskPage() {
     const { data: categories, isLoading: isLoadingCategories } = useQuery({
@@ -12,13 +15,19 @@ export default function KioskPage() {
         queryFn: getCategories,
     });
 
+    //Protect page with correct permissions
+    const router = useRouter();
+    useEffect(() => {
+        protectPage(["Device", "User", "Admin"], router, "/register-device", "/register-device");
+    }, []);
+
     return (
         <main className="flex min-h-screen flex-col items-center p-24">
             <Header>Queue System</Header>
 
             <div className="mt-20 flex w-full flex-col items-center">
                 {isLoadingCategories && <p>Loading...</p>}
-                {categories &&
+                {Array.isArray(categories) &&
                     categories?.map((category: Category) => {
                         return <NumberGetterButton key={category.id} category={category} />;
                     })}
