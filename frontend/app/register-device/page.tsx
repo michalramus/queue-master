0b80f36def199d85b9168840921eed989bcc7fed
Michal Ramus <ramus.michal21@gmail.com>
2024-09-15 10:14:55 +0200

    feat(gui): Create register-device page

---
+++ frontend/app/register-device/page.tsx
@@ -0,0 +1,9 @@
+import RegisterDeviceForm from "./RegisterDeviceForm";
+
+export default function RegisterDevice() {
+    return (
+        <main className="flex min-h-screen flex-col items-center p-24">
+            <RegisterDeviceForm />
+        </main>
+    );
+}
