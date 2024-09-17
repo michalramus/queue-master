0d532d2552a5588b54923226f61e98bc8e5a2654
Michal Ramus <ramus.michal21@gmail.com>
2024-09-17 13:02:27 +0200

    feat(gui): Replace small buttons with icons

---
+++ frontend/components/svg/RejectIcon.tsx
@@ -0,0 +1,19 @@
+export default function RejectIcon() {
+    return (
+        <svg
+            className="h-4 w-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 10 10"
+        >
+            <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.7"
+                d="M1 1 L9 9 M9 1 L1 9"
+            />
+        </svg>
+    );
+}
