b7143088251dd761d312b364512cd188b0b684a9
Michal Ramus <ramus.michal21@gmail.com>
2024-09-17 13:02:27 +0200

    feat(gui): Replace small buttons with icons

---
+++ frontend/components/svg/AcceptIcon.tsx
@@ -0,0 +1,19 @@
+export default function AcceptIcon() {
+    return (
+        <svg
+            className="h-4 w-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+        >
+            <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2.5"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+        </svg>
+    );
+}
