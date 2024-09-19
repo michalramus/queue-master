9a131c22ffaaea558083f69af9fa4f5a0151d22b
Michal Ramus <ramus.michal21@gmail.com>
2024-09-19 12:36:10 +0200

    feat(gui): Redesign TV page

--- frontend/app/tv/ClientNumbersHistoryTable.tsx
+++ frontend/app/tv/ClientNumbersHistoryTable.tsx
@@ -1,38 +1,22 @@
+import Table from "@/components/Table";
 import { ClientNumber } from "../../utils/api/CSR/clients";
-function ClientNumbersHistoryRow({ clientNumber }: { clientNumber: ClientNumber }) {
-    return (
-        <tr className="border-b border-gray-700 odd:bg-gray-900 even:bg-gray-800">
-            <th scope="row" className="whitespace-nowrap px-6 py-4">
-                {clientNumber.number}
-            </th>
-            <td className="px-6 py-4">{clientNumber.seat}</td>
-        </tr>
-    );
-}
+import { ReactNode } from "react";
 
 export default function ClientNumbersHistory({ clientNumbers }: { clientNumbers: ClientNumber[] }) {
+    const rows: (string | number | ReactNode | null)[][] = [];
+    clientNumbers.map((clientNumber) => rows.push([clientNumber.number, clientNumber.seat]));
+
     return (
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-center text-sm text-gray-400">
-                <thead className="bg-gray-700 text-3xl uppercase text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3">
-                            Number
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Seat
-                        </th>
-                    </tr>
-                </thead>
-                <tbody className="text-3xl font-bold text-white">
-                    {clientNumbers.map((clientNumber) => (
-                        <ClientNumbersHistoryRow
-                            key={clientNumber.number}
-                            clientNumber={clientNumber}
-                        />
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <Table
+            columns={[
+                <span className="font-light">Number</span>,
+                <span className="font-light">Seat</span>,
+            ]}
+            rows={rows}
+            theadClassName="text-4xl"
+            tbodyClassName=" text-5xl font-medium"
+            tdBodyClassName="py-3"
+            className="w-6/12"
+        />
     );
 }
