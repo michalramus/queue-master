32b73fbf11c16a223c122ddc06e797689a68301b
Michal Ramus <ramus.michal21@gmail.com>
2024-09-19 16:33:26 +0200

    feat(gui): Redesign register-device page

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
 
-export default function Header() {
+export default function Header({
+    className,
+}: {
+    className?: React.ComponentProps<"div">["className"];
+}) {
     return (
-        <div>
-            <div className="flex w-full items-center justify-center font-semibold">
-                <p className="text-6xl">Queue</p>
-                <Image src="/logo.svg" alt="logo" height={96} width={96} className="mx-2"></Image>
-                <p className="text-6xl">System</p>
+        <div className={`flex flex-col items-center ${className}`}>
+            <div>
+                <div className="flex items-center justify-center font-semibold">
+                    <p className="text-6xl">Queue</p>
+                    <Image
+                        src="/logo.svg"
+                        alt="logo"
+                        height={96}
+                        width={96}
+                        className="mx-2"
+                    ></Image>
+                    <p className="text-6xl">System</p>
+                </div>
+                <p className="text-right text-xl">By Michał Ramus</p>
             </div>
-            <p className="text-right text-xl">By Michał Ramus</p>
         </div>
     );
 }
