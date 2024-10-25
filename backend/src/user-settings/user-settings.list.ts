3747373e54ecf86004dd87445fdfdf1b36f96b45
Michal Ramus <ramus.michal21@gmail.com>
2024-10-25 16:29:18 +0200

    feat(gui): Add scope for setting "seat"

--- backend/src/user-settings/user-settings.list.ts
+++ backend/src/user-settings/user-settings.list.ts
@@ -1,7 +1,7 @@
 import { Setting, SettingSupportedTypes } from "../settings/setting.class";
 
 export const userSettingsList: { [key: string]: Setting<SettingSupportedTypes> } = {
-    seat: new Setting<number>("seat", 1),
+    seat: new Setting<number>("seat", 1, undefined, { min: 1, max: 999 }),
 };
 
 export const userSettingsListKeys = Object.keys(userSettingsList) as Array<keyof typeof userSettingsList>;
