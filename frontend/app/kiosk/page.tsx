6121ed323bd52367297156f982f456d4c40ef97d
Michal Ramus <ramus.michal21@gmail.com>
2024-11-16 18:15:53 +0100

    feat(gui): Add language switcher to kiosk

--- frontend/app/kiosk/page.tsx
+++ frontend/app/kiosk/page.tsx
@@ -1,12 +1,14 @@
 import Header from "../../components/Header";
 import { getCategoriesSSR } from "@/utils/api/SSR/categories";
 import CategoriesForm from "./CategoriesForm";
+import LanguageSwitcher from "@/components/LanguageSwitcher";
 
 export default async function KioskPage() {
     const categories = await getCategoriesSSR();
 
     return (
         <main className="flex min-h-screen flex-col items-center p-16">
+            <LanguageSwitcher />
             <Header />
             <CategoriesForm prefetchCategories={categories} />
         </main>
