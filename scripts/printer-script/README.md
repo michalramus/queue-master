ecdabe96fa2c39d84993f3321bbe65a14fe0c5d4
Michal Ramus <ramus.michal21@gmail.com>
2025-07-17 10:14:55 +0200

    feat: Extend printer script

--- scripts/printer-script/README.md
+++ scripts/printer-script/README.md
@@ -1,10 +1,9 @@
 # Example scripts that can be used to print the tickets
 
-Example script call: ```python3 <print_tickets.py> '{"categoryShortName": string, "number": number, template: string}'```
+Example script call: ```cups-printer-script.py '{"categoryShortName": string, "number": number, template: string}'```
 
 ```template``` is fetched from the server and it could be used to print the ticket.
 
-```printingScriptAddPythonPrefix``` - if false then ```python3``` prefix will be skipped and it will be possible to call binary programs instead of using python. Can be set in kiosk app config
 
 ## cups-printer-script.py
 
@@ -22,6 +21,8 @@ These variables will be replaced with the actual values from the json. Check exa
 
 - ```&categoryShortName``` - categoryShortName from json
 - ```&number``` - number from json
+- ```&date``` - current date in format YYYY-MM-DD
+- ```&time``` - current time in format HH:MM:SS
 
 ### Useful cups commands
 
--- scripts/printer-script/cups-printer-script.py
+++ scripts/printer-script/cups-printer-script.py
@@ -1,24 +1,30 @@
+#!/usr/bin/python3
+
 import os
 import pymupdf
 import sys
 import json
 import cups
+import datetime
 from tempfile import gettempdir
 
-printerName = "printerName" #Printer name provided by cups - check ReadMe 
+printerName = "printerName"  # Printer name provided by cups - check ReadMe
 printerOptions = {
     # 'media': 'A4'
 }
 
 # Define custom paper size that pdf will be generated with
-customWidth = 210  # in mm
-customHeight = 297  # in mm
+customWidth = 58  # in mm
+customHeight = 70  # in mm
 
-pdfMargins = (36, 36, -36, -36)  # Margins (left, top, right, bottom)
+pdfMargins = (0, -13, 0, 0)  # Margins (left, top, right, bottom)
 
-ticketPDFPath = os.path.join(gettempdir(), "ticket.pdf") #Use temp directory, because file is not cleaned up after printing
+ticketPDFPath = os.path.join(
+    gettempdir(), "ticket.pdf"
+)  # Use temp directory, because file is not cleaned up after printing
 
-disablePrint = True # useful for debugging
+disablePrint = False  # useful for debugging
+
 
 def generatePDF(HTML: str):
     story = pymupdf.Story(html=HTML)
@@ -42,6 +48,7 @@ def generatePDF(HTML: str):
         writer.end_page()  # finish page
     writer.close()  # close the writer
 
+
 def printPDF():
     conn = cups.Connection()
 
@@ -52,12 +59,13 @@ def printPDF():
 def main():
 
     inputs = json.loads(sys.argv[1])
-    
-    
+
     HTML = (
         inputs["template"]
         .replace("&categoryShortName", str(inputs["categoryShortName"]))
         .replace("&number", str(inputs["number"]))
+        .replace("&date", str(datetime.datetime.now().strftime("%Y-%m-%d")))
+        .replace("&time", str(datetime.datetime.now().strftime("%H:%M:%S")))
     )
 
     generatePDF(HTML)
@@ -66,8 +74,6 @@ def main():
         printPDF()
     else:
         print("PDF path: ", ticketPDFPath)
-
-
 
 
 if __name__ == "__main__":
