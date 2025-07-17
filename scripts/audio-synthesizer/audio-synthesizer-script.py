4c136c5bf25e7d8c3617daf7030619b178b6dbbe
Michal Ramus <ramus.michal21@gmail.com>
2025-07-17 10:36:02 +0200

    fix(scripts): Add #!/usr/bin/python3 to synthesizer script

--- scripts/audio-synthesizer/audio-synthesizer-script.py
+++ scripts/audio-synthesizer/audio-synthesizer-script.py
@@ -1,15 +1,12 @@
+#!/usr/bin/python3
+
 import os
-import pymupdf
 import sys
 import json
-import cups
-from tempfile import gettempdir
 import subprocess
 
 
-#TODO use RHVoice instead of espeak
 def main():
-
     inputs = json.loads(sys.argv[1])
 
     categoryShortName = str(inputs["categoryShortName"])
@@ -17,13 +14,10 @@ def main():
     seat = str(inputs["seat"])
 
 
-    textToSay = categoryShortName + number + " Seat " + seat
+    textToSay = categoryShortName + number + ", Desk " + seat
 
     result = subprocess.run(["espeak", textToSay], capture_output=True, text=True)
     print(result.stdout)
-
-
-
 
 if __name__ == "__main__":
     main()
