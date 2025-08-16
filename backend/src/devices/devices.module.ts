1c9d0f76b27696ab52e0dff77983384ffcf8189a
Michal Ramus <ramus.michal21@gmail.com>
2025-08-16 13:03:13 +0200

    feat(api): Add users CRUD endpoints

--- backend/src/devices/devices.module.ts
+++ backend/src/devices/devices.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { DevicesService } from "./devices.service";
 import { DevicesController } from "./devices.controller";
 import { DatabaseModule } from "src/database/database.module";
@@ -6,7 +6,7 @@ import { DatabaseModule } from "src/database/database.module";
 import { AuthModule } from "src/auth/auth.module";
 
 @Module({
-    imports: [DatabaseModule, AuthModule],
+    imports: [DatabaseModule, forwardRef(() => AuthModule)],
     providers: [DevicesService],
     controllers: [DevicesController],
     exports: [DevicesService],
--- backend/src/users/types/user.class.ts
+++ backend/src/users/types/user.class.ts
@@ -1,19 +1,11 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { UserRole } from "@prisma/client";
 
 export class User {
-    @ApiProperty({ description: "User ID", example: 1 })
     id: number;
 
-    @ApiProperty({ description: "Username", example: "john_doe" })
     username: string;
 
-    @ApiProperty({ description: "User password", example: "hashed_password" })
     password: string;
 
-    @ApiProperty({
-        description: "User role",
-        enum: ["Device", "User", "Admin"],
-        example: "User",
-    })
-    role: "Device" | "User" | "Admin";
+    role: UserRole;
 }
