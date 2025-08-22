e480db3efdf5a2df1126e70e9372c3b5e30f978a
Michal Ramus <ramus.michal21@gmail.com>
2025-08-22 16:12:40 +0200

    feat: Add support for disabling and overriding opening hours feature

--- backend/src/settings/types/booleanSettingValue.class.ts
+++ backend/src/settings/types/booleanSettingValue.class.ts
@@ -23,11 +23,7 @@ export class BooleanSettingValue extends CustomSettingValue {
         return new BooleanSettingValue(value === "true");
     }
 
-    toString(): string {
-        return this.defaultValue.toString();
-    }
-
-    toJSON(): string {
-        return this.defaultValue.toString();
+    toJSON(): any {
+        return this.defaultValue;
     }
 }
